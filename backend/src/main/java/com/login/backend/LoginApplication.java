593362fbfb62febcd14642c80c22cffd721d9f3f
EddinKahvic <eddin.kahvic@hotmail.com>
2025-05-13 00:39:00 +0200

    feat: Initial landing page (returns Hello)

--- backend/src/main/java/com/login/backend/LoginApplication.java
+++ backend/src/main/java/com/login/backend/LoginApplication.java
@@ -1,4 +1,4 @@
-package com.login.login;
+package com.login.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend/src/main/java/com/login/backend/authentication/controller/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.login.backend.authentication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello!";
+    }
+}

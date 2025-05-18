1a4e03b8598916be461fc973519395ec7eb38085
EddinKahvic <eddin.kahvic@hotmail.com>
2025-05-18 16:07:27 +0200

    feat: Can now search for users when providing email in url

--- backend/src/main/java/com/login/backend/features/authentication/controller/AuthenticationController.java
+++ backend/src/main/java/com/login/backend/features/authentication/controller/AuthenticationController.java
@@ -2,14 +2,24 @@ package com.login.backend.features.authentication.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.login.backend.features.authentication.model.AuthenticationUser;
+import com.login.backend.features.authentication.service.AuthenticationService;
 
 @RestController
 @RequestMapping("/api/v1/authentication")
 public class AuthenticationController {
 
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
     @GetMapping("/user")
-    public String getUser() {
-        return "User";
+    public AuthenticationUser getUser(@RequestParam String email) {
+        return authenticationService.getUser(email);
     }
 }
--- backend/src/main/java/com/login/backend/features/authentication/repository/AuthenticationUserRepository.java
+++ backend/src/main/java/com/login/backend/features/authentication/repository/AuthenticationUserRepository.java
@@ -1,8 +1,10 @@
 package com.login.backend.features.authentication.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.login.backend.features.authentication.model.AuthenticationUser;
 
 public interface AuthenticationUserRepository extends JpaRepository<AuthenticationUser, Long>{
-
+    Optional<AuthenticationUser> findByEmail(String emaill);
 }

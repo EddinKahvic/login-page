a1622993e575e3d207cd5c457f987e1510ce343c
EddinKahvic <eddin.kahvic@hotmail.com>
2025-05-30 00:38:37 +0200

    chore: initial setup of environment and route creation

---
+++ frontend/src/features/authentication/verifyEmail/verifyEmail.tsx
@@ -0,0 +1,3 @@
+export default function verifyEmail() {
+  return <div>verifyEmail</div>
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.scss'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Feed from './features/feed/pages/Feed'
+import Login from './features/authentication/login/login'
+import Signup from './features/authentication/signup/signup'
+import ResetPassword from './features/authentication/resetPassword/resetPassword'
+import VerifyEmail from './features/authentication/verifyEmail/verifyEmail'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Feed />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/request-password-reset',
+    element: <ResetPassword />,
+  },
+  {
+    path: '/verify-email',
+    element: <VerifyEmail />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
